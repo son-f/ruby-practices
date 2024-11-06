051c903946038e6efcdbea135b7e0fba0a4b2829
son-f <sfvocakmkanosin@ezweb.ne.jp>
2024-11-06 21:26:41 +0900

    修正2回目1106

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,39 +1,27 @@
 #!/usr/bin/env ruby
 
 require 'date'
-require "optparse"
+require 'optparse'
 
 opt = OptionParser.new
 argv_date = {}
-opt.on('-y [DATE]') {|v| v } 
-opt.on('-m [DATE]') {|v| v } 
+opt.on('-y [DATE]') { |v| v }
+opt.on('-m [DATE]') { |v| v }
 opt.parse!(ARGV, into: argv_date)
 
-if argv_date[:y]
-  year = argv_date[:y].to_i
-else
-  year = Date.today.year
-end
-if argv_date[:y]
-  month = argv_date[:m].to_i
-else
-  month = Date.today.month
-end
+year = argv_date[:y] ? argv_date[:y].to_i : Date.today.year
+month = argv_date[:y] ? argv_date[:m].to_i : Date.today.month
 
-puts "#{month}月 #{year}".center(20) 
-puts "日 月 火 水 木 金 土"
+puts "#{month}月 #{year}".center(20)
+puts '日 月 火 水 木 金 土'
 
 start_date = Date.new(year, month)
 end_date = Date.new(year, month, -1)
 
-print "   "  * start_date.cwday 
+print '   ' * start_date.wday
 
 (start_date..end_date).each do |date|
-  everyday = date.strftime('%e') + " "
-  if date.saturday?
-    puts everyday
-  else
-    print everyday
-  end
+  print date.strftime('%e')
+  date.saturday? ? (print "\n") : (print ' ')
 end
-print "\n" 
+print "\n"
